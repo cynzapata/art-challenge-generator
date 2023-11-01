d3f98487403da5febb135c7397b71d23bb704839
Cynthia Zapata <cynthiazapata2017@gmail.com>
2023-11-01 00:17:29 -0500

    added text and reload icon

--- src/components/RandomChallenge/RandomChallenge.jsx
+++ src/components/RandomChallenge/RandomChallenge.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './RandomChallenge.css'
+import reload_icon from '../Assets/reload.png'
 
 export const RandomChallenge = () => {
-  return (
-    <div>
-        
+  
+    const[challenge,setChallenge] = useState({
+        text: "Draw a frog with a strawberry"
+    });
+    return (
+
+    <div className='container'>
+        <div className="challenge">{challenge.text}</div>
+        <div>
+            <div className='line'></div>
+            <div className="bottom"></div>
+            <div className="icons">
+                <img src={reload_icon} alt="reload icon" />
+    
+            </div>
+        </div>
     </div>
   )
 }
